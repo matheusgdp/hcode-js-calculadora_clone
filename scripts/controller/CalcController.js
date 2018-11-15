e908445eb52b8d62de7e8accc7330b3153caf17d
Matheus Guimarães <matheusgdp91@gmail.com>
2018-11-15 20:14:40 -0200

    create inicialize and constructor in class CalcController

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,7 +3,18 @@ class CalcController {
     constructor(){
         this._displayCalc = '0';
         this.currentDate; 
+        this.initialize();
     }
+
+    initialize(){
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displayCalcEl.innerHTML = '15000';
+        dateEl.innerHTML = '15/11/2018';
+        timeEl.innerHTML = '00:00';
+    };
 
     get displayCalc(){
         return this._displayCalc;
